a112a82c98289ab883bf4b1e908af9afb5d62454
NatalieGichuki <nataliegichuki@gmail.com>
2025-06-17 11:52:27 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -16,11 +16,61 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.desc()).all()
+    response = make_response(jsonify([message.to_dict() for message in messages]), 200)
+
+    return response
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    messages = Message.query.filter_by(id = id).all()
+    if messages:
+        response = make_response(jsonify([message.to_dict() for message in messages]), 200)
+        return response
+    else:
+        return make_response(jsonify({"error": "Message not found"}), 404)
+
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    body = data.get('body')
+    username = data.get('username')
+
+    if not body or not username:
+        return make_response(jsonify({"error": "Body and Username are required"}), 400)
+    
+    new_message = Message(body=body, username=username)
+    db.session.add(new_message)
+    db.session.commit()
+    response = make_response(jsonify(new_message.to_dict()), 201)
+    return response
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.filter_by(id = id).first()
+    if not message:
+        return make_response(jsonify({"error": "Message not found"}), 404)
+    
+    data = request.get_json()
+    new_body = data.get('body')
+
+    if new_body:
+        message.body = new_body
+        db.session.commit()
+
+    response = make_response(jsonify(message.to_dict()), 200)
+    return response
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.filter_by(id = id).first()
+    if not message:
+        return make_response(jsonify({"error": "Message not found"}), 404)
+    
+    db.session.delete(message)
+    db.session.commit()
+    return make_response(jsonify({"message": "Message deleted successfully"}), 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
